83f50cfd9ef9a9ac46e17907c7aa26dc2e4083b5
Steve Kemp <steve@steve.org.uk>
2014-03-13 13:06:31 +0000

    Test that sshd is running on :2222.

--- spec/web4.debian-administration.org/web_spec.rb
+++ spec/web4.debian-administration.org/web_spec.rb
@@ -30,3 +30,19 @@ describe default_gateway do
   its(:ipaddress) { should eq '212.110.179.65' }
 end
 
+
+
+#
+#  SSH testing, which runs on :2222
+#
+describe service('ssh') do
+  it { should be_enabled   }
+  it { should be_running   }
+end
+describe file('/etc/ssh/sshd_config') do
+  it { should be_file }
+  its(:content) { should match /Port\s+2222/ }
+end
+describe port(2222) do
+  it { should be_listening }
+end
